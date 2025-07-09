604d1f860039e05776f953b1e9c097fe1b7f2544
Raul Silva <raul.silva@kaxanuk.mx>
2025-07-09 12:16:51 -0600

    Simple Backtester initial

---
+++ src/kaxanuk/attribution_analysis/entinties/__init__.py
@@ -0,0 +1,5 @@
+from kaxanuk.attribution_analysis.entinties.naive_backtest import NaiveBacktest
+
+__all__ = [
+    'NaiveBacktest'
+]
---
+++ src/kaxanuk/attribution_analysis/interfaces/__init__.py
@@ -0,0 +1,9 @@
+from src.kaxanuk.attribution_analysis.interfaces.strategy import StrategyBacktest
+from src.kaxanuk.attribution_analysis.interfaces.brinston_fachler import BrinstonFachlerInterface
+from src.kaxanuk.attribution_analysis.interfaces.capm_interfase import CapmAttributionInterfase
+
+__all__ = [
+    'StrategyBacktest',
+    'BrinstonFachlerInterface',
+    'CapmAttributionInterfase'
+]
